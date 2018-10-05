ba485373f23311721cd43aa0e627945cbb625374
Chi-TIng Chiang <cchiang@astro0010.rcf.bnl.gov>
2018-10-05 13:54:01 -0400

    10/5 edit

--- gen_image.py
+++ gen_image.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from sim import ImageMaker
 import yaml
 
-fin = open('simplest.config','r')
+fin = open('config_files/simplest.config','r')
 config = yaml.safe_load(fin)
 maker = ImageMaker(config)
 
@@ -11,7 +11,11 @@ for i in range(5):
     one_img = maker.get_one()
     scene_img = maker.get_scene()
     print one_img.shape,scene_img.shape
-    plt.figure(i+1,figsize=(5.5,5))
+    plt.figure(i+1,figsize=(10,5))
+    plt.subplot(1,2,1)
     plt.pcolor(one_img,cmap='jet')
+    plt.tight_layout()
+    plt.subplot(1,2,2)
+    plt.pcolor(scene_img,cmap='jet')
     plt.tight_layout()
     plt.show()
